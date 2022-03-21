3f8646cc577195f1f4a425a0f62575b590da232e
glorialea <glorialeadwikurnia@gmail.com>
2022-03-22 00:44:35 +0700

    first commit

---
+++ src/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    //$app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+      //  $container->get('logger')->info("Slim-Skeleton '/' route");
+
+        // Render index view
+        //return $container->get('renderer')->render($response, 'index.phtml', $args);
+    //});
+
+    $app->get("/api/kelas", function (Request $request, Response $response){
+        $sql = "SELECT * FROM kelas";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+    $app->get("/api/kelas/{idkelas}", function (Request $request, Response $response, $args){
+        $idkelas = $args["idkelas"];
+        $sql = "SELECT * FROM kelas WHERE id_kelas=:id_kelas";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([":id_kelas" => $idkelas]);
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+};
